33cc41c1b9fa1c1cd51c1c5925e6c8d7d181fbde
lyzboy <13478602+lyzboy@users.noreply.github.com>
2024-02-24 16:52:24 -0700

    Add props and states to ContactsPage component

--- src/containers/contactsPage/ContactsPage.js
+++ src/containers/contactsPage/ContactsPage.js
@@ -3,34 +3,37 @@ import React, { useState, useEffect } from "react";
 import { ContactForm } from "../../components/contactForm/ContactForm";
 import { TileList } from "../../components/tileList/TileList";
 
-export const ContactsPage = () => {
-  /*
+export const ContactsPage = ({ contacts, addContacts }) => {
+    /*
   Define state variables for 
   contact info and duplicate check
   */
+    const [name, setName] = useState(``);
+    const [phoneNumber, setPhoneNumber] = useState(``);
+    const [email, setEmail] = useState(``);
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    /*
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        /*
     Add contact info and clear data
     if the contact name is not a duplicate
     */
-  };
+    };
 
-  /*
+    /*
   Using hooks, check for contact name in the 
   contacts array variable in props
   */
 
-  return (
-    <div>
-      <section>
-        <h2>Add Contact</h2> 
-      </section>
-      <hr />
-      <section>
-        <h2>Contacts</h2>
-      </section>
-    </div>
-  );
+    return (
+        <div>
+            <section>
+                <h2>Add Contact</h2>
+            </section>
+            <hr />
+            <section>
+                <h2>Contacts</h2>
+            </section>
+        </div>
+    );
 };
