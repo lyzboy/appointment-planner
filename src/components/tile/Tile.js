9081eb93dc5c259abb20a0affd806925d39919d9
lyzboy <13478602+lyzboy@users.noreply.github.com>
2024-02-25 21:24:23 -0700

    Refactor Tile component to display name and description

--- src/components/tile/Tile.js
+++ src/components/tile/Tile.js
@@ -1,9 +1,16 @@
 import React from "react";
 
-export const Tile = () => {
-  return (
-    <div className="tile-container">
-      
-    </div>
-  );
+export const Tile = ({ name, description }) => {
+    return (
+        <div className="tile-container">
+            <p className="tile-title">{name}</p>
+            {Object.entries(description).map(([key, value]) => {
+                return (
+                    <p className="tile" key={key}>
+                        {value}
+                    </p>
+                );
+            })}
+        </div>
+    );
 };
