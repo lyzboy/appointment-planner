d3dfd72a32377fcf87f001cca260dfbba00dd8ac
lyzboy <13478602+lyzboy@users.noreply.github.com>
2024-02-25 20:01:22 -0700

    Add formatPhoneNumber function to contactsUtilities.js

--- src/utils/contactsUtilities.js
+++ src/utils/contactsUtilities.js
@@ -10,3 +10,21 @@ export const contactsContainName = (contactArray, contactName) => {
     );
     return searchElements.length !== 0;
 };
+
+export const formatPhoneNumber = (phoneNumber) => {
+    const sanitizedPhoneNumber = phoneNumber.replace(/\D/g, "");
+    let formattedPhoneNumber = ``;
+    if (sanitizedPhoneNumber.length > 0) {
+        formattedPhoneNumber = `(`;
+        formattedPhoneNumber += sanitizedPhoneNumber.slice(0, 3);
+        if (sanitizedPhoneNumber.length > 3) {
+            formattedPhoneNumber += `)`;
+            formattedPhoneNumber += sanitizedPhoneNumber.slice(3, 6);
+            if (sanitizedPhoneNumber.length > 6) {
+                formattedPhoneNumber += `-`;
+                formattedPhoneNumber += sanitizedPhoneNumber.slice(6);
+            }
+        }
+    }
+    return formattedPhoneNumber;
+};
--- src/utils/contactsUtilities.test.js
+++ src/utils/contactsUtilities.test.js
@@ -1,5 +1,5 @@
 import "./contactsUtilities";
-import { contactsContainName } from "./contactsUtilities";
+import { contactsContainName, formatPhoneNumber } from "./contactsUtilities";
 
 describe("The contacts utilities helper", () => {
     describe(`contactsContainName function`, () => {
@@ -20,6 +20,28 @@ describe("The contacts utilities helper", () => {
             const actualValue = contactsContainName(inputArray, inputValue);
 
             expect(actualValue).toEqual(expectValue);
+        });
+    });
+    describe("formatPhoneNumber function", () => {
+        it("formats a give phone number in the form of (***)***-****", () => {
+            const inputPhone1 = "1234567890";
+            const expectedPhone1 = "(123)456-7890";
+            const inputPhone2 = "123456";
+            const expectedPhone2 = "(123)456";
+            const inputPhone3 = "123";
+            const expectedPhone3 = "(123";
+            const inputPhone4 = "1";
+            const expectedPhone4 = "(1";
+
+            const actualValue1 = formatPhoneNumber(inputPhone1);
+            const actualValue2 = formatPhoneNumber(inputPhone2);
+            const actualValue3 = formatPhoneNumber(inputPhone3);
+            const actualValue4 = formatPhoneNumber(inputPhone4);
+
+            expect(actualValue1).toEqual(expectedPhone1);
+            expect(actualValue2).toEqual(expectedPhone2);
+            expect(actualValue3).toEqual(expectedPhone3);
+            expect(actualValue4).toEqual(expectedPhone4);
         });
     });
 });
