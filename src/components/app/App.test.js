bfcd44c67abe0235d05fa67ea417f83d3d66a69c
lyzboy <13478602+lyzboy@users.noreply.github.com>
2024-02-26 21:43:54 -0700

    Add tests for creating and preventing duplicate contacts

--- src/components/app/App.test.js
+++ src/components/app/App.test.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import { MemoryRouter } from "react-router-dom";
 import App, { AppRoutes } from "./App";
 import { ContactsPage } from "../../containers/contactsPage/ContactsPage";
@@ -50,5 +50,74 @@ describe("The AppRoutes component", () => {
         const heading = screen.getByText(/contacts/i);
 
         expect(heading).toBeInTheDocument();
+    });
+
+    it(`renders a new contact when a new contact is added`, () => {
+        const { getByLabelText, getByText, container } = render(<App />);
+
+        const expectedName = `John Doe`;
+        const expectedPhone = `(123)456-7890`;
+        const expectedEmail = `john.doe@example.com`;
+
+        // Fill out the form
+        fireEvent.change(getByLabelText(`Name:`), {
+            target: { value: "John Doe" },
+        });
+        fireEvent.change(getByLabelText(`Phone:`), {
+            target: { value: "1234567890" },
+        });
+        fireEvent.change(getByLabelText(`Email:`), {
+            target: { value: "john.doe@example.com" },
+        });
+
+        // Submit the form
+        fireEvent.click(getByText(`Create Contact`));
+
+        const contactNames = container.querySelectorAll(`.tile`);
+        const matchingNames = Array.from(contactNames).filter(
+            (node) => node.textContent === expectedName
+        );
+
+        expect(matchingNames.length).toBe(1);
+        expect(getByText(expectedPhone)).toBeInTheDocument();
+        expect(getByText(expectedEmail)).toBeInTheDocument();
+    });
+
+    it(`does not allow for duplicate contacts to be created`, () => {
+        const { getByLabelText, getByText, getAllByText, container } = render(
+            <App />
+        );
+
+        const expectedName = `John Doe`;
+        const expectedPhone = `(123)456-7890`;
+        const expectedEmail = `john.doe@example.com`;
+
+        const fillOutForm = () => {
+            // Fill out the form
+            fireEvent.change(getByLabelText(`Name:`), {
+                target: { value: "John Doe" },
+            });
+            fireEvent.change(getByLabelText(`Phone:`), {
+                target: { value: "1234567890" },
+            });
+            fireEvent.change(getByLabelText(`Email:`), {
+                target: { value: "john.doe@example.com" },
+            });
+        };
+        fillOutForm();
+        // Submit the form
+        fireEvent.click(getByText(`Create Contact`));
+        fillOutForm();
+        // Submit the form
+        fireEvent.click(getByText(`Create Contact`));
+
+        const contactNames = container.querySelectorAll(`.tile`);
+        const matchingNames = Array.from(contactNames).filter(
+            (node) => node.textContent === expectedName
+        );
+
+        expect(matchingNames.length).toBe(1);
+        expect(getAllByText(expectedPhone).length).toBe(1);
+        expect(getAllByText(expectedEmail).length).toBe(1);
     });
 });
