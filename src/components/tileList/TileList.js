770e28c44967177020b875eabab5d2e60626b444
lyzboy <13478602+lyzboy@users.noreply.github.com>
2024-02-27 20:02:10 -0700

    Add styling to TileList component

--- src/components/tileList/TileList.js
+++ src/components/tileList/TileList.js
@@ -1,15 +1,16 @@
 import React from "react";
 import { Tile } from "../tile/Tile";
 
+import styles from "./TileList.module.css";
+
 export const TileList = ({ list }) => {
-    console.log(list);
     return (
-        <>
+        <div className={styles.tileList}>
             {list.map((elem) => {
                 return (
                     <Tile key={elem.name} name={elem.name} description={elem} />
                 );
             })}
-        </>
+        </div>
     );
 };
