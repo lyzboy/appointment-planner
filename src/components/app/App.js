1ff4fcd1b4412ef0c0f66f414e2ba2317cdf63fb
lyzboy <13478602+lyzboy@users.noreply.github.com>
2024-02-27 20:01:42 -0700

    Add useEffect hook to set initial contacts for development

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import {
     BrowserRouter as Router,
     Routes,
@@ -22,6 +22,21 @@ export function AppRoutes() {
         const newAppointment = { name, contact, date, time };
         setAppointments((prev) => [...prev, newAppointment]);
     };
+
+    useEffect(() => {
+        setContacts([
+            {
+                name: "John Doe",
+                phone: "(123)123-1234",
+                email: "john@example.com",
+            },
+            {
+                name: "Tom Doe",
+                phone: "(123)123-1235",
+                email: "tom@example.com",
+            },
+        ]);
+    }, []);
 
     return (
         <Routes>
