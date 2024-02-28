42a2fb245aad4dd8fa170766b8e55f3eb695f9ca
lyzboy <13478602+lyzboy@users.noreply.github.com>
2024-02-27 20:02:03 -0700

    Add styles to Tile component

--- src/components/tile/Tile.js
+++ src/components/tile/Tile.js
@@ -1,9 +1,11 @@
 import React from "react";
 
+import styles from "./Tile.module.css";
+
 export const Tile = ({ name, description }) => {
     return (
-        <div className="tile-container">
-            <p className="tile-title">{name}</p>
+        <div className={`tile-container ${styles.tile}`}>
+            <p className={`tile-title ${styles.tile_title}`}>{name}</p>
             {Object.entries(description).map(([key, value]) => {
                 return (
                     <p className="tile" key={key}>
