e66092fc0ccf3e364ab20752eaf63f3f96ab1378
lyzboy <13478602+lyzboy@users.noreply.github.com>
2024-02-28 19:02:04 -0700

    Update index.css and Root.js

--- src/components/root/Root.js
+++ src/components/root/Root.js
@@ -1,25 +1,21 @@
-import {  Outlet, NavLink } from "react-router-dom";
+import { Outlet, NavLink } from "react-router-dom";
 
 export const ROUTES = {
     CONTACTS: "/contacts",
     APPOINTMENTS: "/appointments",
-  };
+};
 
 function Root() {
     return (
         <>
+            <h1>Appointment Planner</h1>
             <nav>
-                <NavLink to={ROUTES.CONTACTS} >
-                Contacts
-                </NavLink>
-                <NavLink to={ROUTES.APPOINTMENTS} >
-                Appointments
-                </NavLink>
+                <NavLink to={ROUTES.CONTACTS}>Contacts</NavLink>
+                <NavLink to={ROUTES.APPOINTMENTS}>Appointments</NavLink>
             </nav>
-            <Outlet/>
-      </>
+            <Outlet />
+        </>
     );
-
 }
 
 export default Root;
